d6ec7537022c48cc471a12a6ddb864eaeef0bceb
Uppara12 <uppararajarajeswari@gmail.com>
2026-05-07 15:38:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ let deferredPrompt;
 
 window.addEventListener("beforeinstallprompt", async (e) => {
 
-  // Prevent mini infobar
+  // Stop default install mini-infobar
   e.preventDefault();
-
-  console.log("beforeinstallprompt triggered");
 
   // Save event
   deferredPrompt = e;
 
+  // Immediately show install popup
   if (deferredPrompt) {
 
     deferredPrompt.prompt();
 
+    // Wait for user action
     const result = await deferredPrompt.userChoice;
 
-    console.log("User Choice:", result.outcome);
+    console.log("User choice:", result.outcome);
 
     deferredPrompt = null;
   }
